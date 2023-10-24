31ce02baba2520b674e34e04013621b5ad1a5936
Giovanne Corrêa <giovannecorrea@gmail.com>
2023-10-23 23:00:00 -0300

    test

--- utils/index.ts
+++ utils/index.ts
@@ -6,8 +6,6 @@ export async function fetchCars() {
       'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
   }
 
-  console.log(typeof headers);
-
   const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {headers: headers});
 
   const result = await response.json();
