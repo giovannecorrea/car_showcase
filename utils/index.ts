4b9a31a53ec2a9247b86e0ce0da6fcdf85315d8b
Giovanne Corrêa <giovannecorrea@gmail.com>
2023-10-26 23:14:40 -0300

    finishing search bar

--- utils/index.ts
+++ utils/index.ts
@@ -1,6 +1,8 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
+
+  const { manufacturer, year, model, limit, fuel } = filters;
 
 
   const headers: { 'X-RapidAPI-Key'?: string; 'X-RapidAPI-Host': string } = {
@@ -8,7 +10,7 @@ export async function fetchCars() {
       'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
   }
 
-  const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3', {headers: headers});
+  const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`, {headers: headers});
 
   const result = await response.json();
 
